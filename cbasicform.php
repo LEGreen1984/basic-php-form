b6f0fc2d6fcc70e97266bc790862bbf3024867b5
Computer Setup <CSetup@spartaglobal.com>
2020-02-12 12:05:02 +0000

    exercise 2 complete

--- cbasicform.php
+++ cbasicform.php
@@ -1,6 +1,48 @@
 <?php
-    if (isset($_POST['searchterm'])){
-        echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES);
+    $name = '';
+    $password = '';
+    $gender = '';
+    $colour = '';
+    $languages = [];
+    $comments = '';
+    $tc = '';
+    if (isset($_POST['submit'])){
+        if (isset($_POST['name'])) {
+            $name = $_POST['name'];
+        };
+        if (isset($_POST['password'])) {
+            $password = $_POST['password'];
+        };
+        if (isset($_POST['gender'])) {
+            $gender = $_POST['gender'];
+        };
+        if (isset($_POST['colour'])) {
+            $colour = $_POST['colour'];
+        };
+        if (isset($_POST['languages'])) {
+            $languages = $_POST['languages'];
+        };
+        if (isset($_POST['comments'])) {
+            $comments = $_POST['comments'];
+        };
+        if (isset($_POST['tc'])) {
+            $tc = $_POST['tc'];
+        };
+
+        printf('User name: %s
+        <br>Password: %s
+        <br>Gender: %s
+        <br>Colour: %s
+        <br>Language(s): %s
+        <br>Comments: %s
+        <br>Terms&ampConditions: %s',
+        htmlspecialchars($name, ENT_QUOTES),
+        htmlspecialchars($password, ENT_QUOTES),
+        htmlspecialchars($gender, ENT_QUOTES),
+        htmlspecialchars($colour, ENT_QUOTES),
+        htmlspecialchars(implode(' ',$languages), ENT_QUOTES),
+        htmlspecialchars($comments, ENT_QUOTES),
+        htmlspecialchars($tc, ENT_QUOTES));
     }
 ?>
 
@@ -30,5 +72,5 @@
     Comments: <textarea name="comments"></textarea><br>
     <input type="checkbox" name="tc" value="ok">
         I accept the Terms&amp;Conditions<br>
-    <input type="submit" value="search">
+    <input type="submit" name="submit" value="register">
 </form> 
